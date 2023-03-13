dde0c9bd5201f98e743d87c21f316bf948fe1b5d
Herr Martinlæ <martinfernandoortiz@gmail.com>
2023-03-13 09:58:39 -0300

    Actualización con lectura de grupo de capas geoserver

--- chat_gpt.R
+++ chat_gpt.R
@@ -7,6 +7,17 @@ library(tidyverse)
 setwd("D:/martin/ign-argentina/SLD_reader/SLD_reader")
 
 
+
+#Esto sale del geoserver
+orden_Capas <- read_delim("orden_Capas.txt", 
+                          delim = "\t", escape_double = FALSE, 
+                          trim_ws = TRUE)
+
+
+orden_Capas <- orden_Capas %>% mutate(Estilo = paste0(Estilo,".sld"))
+
+
+
 ############################## FX PARA ZOOM SCALE #############################################
 
 #df <- data.frame(
@@ -23,7 +34,7 @@ get_zoom_level <- function(scale_value, zoomLevel) {
 
 
 ############################### Crear df vacio ############################## 
-columns = c("file_name", "rule_name","rule_desc",
+columns = c("capa","orden_geoserver","file_name", "rule_name","rule_desc",
             "filter","min_scale_denom","max_scale_denom","max_zoom","min_zoom",  
             "symbolizer","PointSymbolizer","LineSymbolizer","PolygonSymbolizer",
             "TextSymbolizer")
@@ -40,17 +51,36 @@ sld_dir <- getwd()
 
 ############################## # Lista de sld
 #sld_files <- list.files(paste0(sld_dir,"/argenmap/"), pattern = "\\.sld$", full.names = TRUE) #chequear dnd están
-sld_files <- list.files(paste0(sld_dir,"/"), pattern = "\\.sld$", full.names = TRUE)
-sld_count <- length(sld_files)
+#sld_files <- list.files(paste0(sld_dir,"/"), pattern = "\\.sld$", full.names = TRUE)
+#sld_count <- length(sld_files)
+
+
+
+sld_folder <- as.character("D:/martin/ign-argentina/github/estilos-sld/argenmap/")
+
+
 
 
 
 #Primer Loop que lee los archivos de la carpeta
-for (xx in 1:13) {
-  
-print(paste("SLD NUMERO",xx, se=" ")) #ESTO ES UN INDICATIVO
 
-sld_xml <- read_xml(sld_files[xx]) #LEO UNO A UNO ARCHIVO DE LA CARPETA
+###############for (xx in 1:13) {        VERSION VIEJA DE PRUEBA. EL LOOP DE ABAJO ES EL QUE VA
+
+
+for (xx in seq_len(nrow(orden_Capas))) {  
+print(paste("SLD NUMERO",xx, se=" ")) #ESTO ES UN INDICATIVO
+print(as.character(orden_Capas[xx,3]))
+
+sld_file <-  as.character(orden_Capas[xx,3])
+sld_xml <- read_xml(paste0(sld_folder,sld_file))
+
+orden_geoserver <- as.character(orden_Capas[xx,1])
+capa <- as.character(orden_Capas[xx,2])
+file_name <- as.character(orden_Capas[xx,3])
+
+
+###############################################sld_xml <- read_xml(sld_files[xx]) VERSION VIEJA
+
 if( as.character(sld_xml)%>% str_detect("xmlns:se") == TRUE) { #ESTO PARA VER EL TIPO DE ESCRITURA SLD
    
     
@@ -70,7 +100,7 @@ for (i in 1:count_rule) {
   
   print(i)
   rules <- xml_find_first(sld_xml, "//se:Rule")
-  file_name  <-  basename(sld_files[xx])
+  #file_name  <-  basename(sld_files[xx])
   
   rule_name <- xml_text(xml_find_first(rules, ".//se:Name"))
   rule_desc <- xml_text(xml_find_first(rules, ".//se:Description/se:Title"))
@@ -127,6 +157,8 @@ for (i in 1:count_rule) {
   
   ##############UNIMOS TODO PARA PASAR A DF
   result[[i]] <- list(
+    capa = capa,
+    orden_geoserver= orden_geoserver,
     file_name = file_name,
     rule_name = rule_name,
     rule_desc = rule_desc,
@@ -153,11 +185,6 @@ df <- rbind(df,result_df)
 }
 
 
-library(readr)
-orden_Capas <- read_delim("orden_Capas.txt", 
-                          delim = "\t", escape_double = FALSE, 
-                          trim_ws = TRUE)
-
 
 
 ########################################## AGREGAR REGISTROS POR NIVEL DE ZOOM ###################
@@ -175,6 +202,9 @@ new_rows <- data.frame(id = numeric(), number = numeric())
 
 # Loop through each row in the original table
 for (i in seq_len(nrow(original_table))) {
+  
+  print(i)
+  
   # Calculate the difference between min and max
   diff <- original_table$max_zoom[i] - original_table$min_zoom[i]
   
@@ -195,3 +225,4 @@ for (i in seq_len(nrow(original_table))) {
 
 
 df_completo <- new_rows %>% left_join(df, by = "id")
+df_3 <- df_completo %>%filter(number ==3)
